7e2a3230c15e1af550350b54f9da0d9b2bdc4a2f
kubealex <al.rossi87@gmail.com>
2022-07-29 01:09:39 +0200

    rhel-gnostic images

--- terraform/modules/01_idm_server/variables.tf
+++ terraform/modules/01_idm_server/variables.tf
@@ -25,7 +25,7 @@ variable cpu {
 variable kickstart_image_idm_server {
   type        = string
   description = "Path for the kickstart image for RHEL8"
-  default     = "oemdrv.img"
+  default     = "rhel-idm-oemdrv.img"
 }
 
 variable libvirt_network {
@@ -49,5 +49,5 @@ variable disk_size {
 variable os_image_idm_server {
   type        = string
   description = "URL/path of the image to be used for the VM provisioning"
-  default     = "rhel8.iso"
+  default     = "rhel.iso"
 }
